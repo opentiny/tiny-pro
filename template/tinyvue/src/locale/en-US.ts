e17212552aac7f07def9640fe7e6527d26b4d7b2
liukun <953831480@qq.com>
2025-04-09 10:26:20 +0800

    fix:还原缺失国际化 (#13)

--- template/tinyvue/src/locale/en-US.ts
+++ template/tinyvue/src/locale/en-US.ts
@@ -1,4 +1,9 @@
+import localeLogin from '@/views/login/locale/en-US';
+import localeHttpError from './en-US/httpError';
+
 export default {
+  ...localeLogin,
+  ...localeHttpError,
   'router.not-exists-valid-route':
     'Route encountered an exception, please contact the administrator',
 };
