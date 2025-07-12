bf8347b485cb15922ef5070ad93f1721ddbf4b19
GaoNeng-wWw <gaonenggg@outlook.com>
2025-07-12 12:38:31 +0800

    refactor(nestJs): restore synchronize

--- template/nestJs/libs/db/src/db.service.ts
+++ template/nestJs/libs/db/src/db.service.ts
@@ -14,6 +14,7 @@ export class DbService implements TypeOrmOptionsFactory {
       username: process.env.DATABASE_USERNAME,
       dropSchema: false, // 为true时启动时删除表结构
       password: process.env.DATABASE_PASSWORD,
+      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
       database: process.env.DATABASE_NAME,
       autoLoadEntities: process.env.DATABASE_AUTOLOADENTITIES === 'true',
     };
