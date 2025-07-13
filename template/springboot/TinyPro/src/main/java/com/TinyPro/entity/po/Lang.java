9932f7dd8745bbdd08e63af624f4e9fc1bc06ad8
zzl <3196630659@qq.com>
2025-07-13 23:01:46 +0800

    登录退出操作，国际化文件加载，数据库初始化操作

--- template/springboot/TinyPro/src/main/java/com/TinyPro/entity/po/Lang.java
+++ template/springboot/TinyPro/src/main/java/com/TinyPro/entity/po/Lang.java
@@ -11,48 +11,16 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
+@Table(name = "lang")
+@Data
 public class Lang implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @TableId(type = IdType.AUTO)
     private Integer id;
 
-    @Column(nullable = false)
     private String name;
 
-    // 假设 Lang 和 I18 是一对多关系
-    @OneToMany( targetEntity = I18.class, fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST})
-    @JsonIgnoreProperties(value = {"1"})
-    @JoinColumn(name = "lang_id",
-            referencedColumnName = "id",
-            insertable = false,
-            updatable = false )
-    private List<I18> i18;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<I18> getI18() {
-        return i18;
-    }
-
-    public void setI18(List<I18> i18) {
-        this.i18 = i18;
-    }
+    @OneToMany(mappedBy = "lang", fetch = FetchType.LAZY)
+    private List<I18> i18ns;
 }
--- template/springboot/TinyPro/src/main/java/com/TinyPro/entity/po/Role.java
+++ template/springboot/TinyPro/src/main/java/com/TinyPro/entity/po/Role.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,57 +18,22 @@ import java.util.List;
 public class Role implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @TableId(type = IdType.AUTO)
     private Integer id;
 
-    @Column(nullable = false, unique = true) // 角色名称唯一且非空
     private String name;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(
-            name = "role_permission",
-            joinColumns = @JoinColumn(name = "roleId"), // 改为蛇形命名
-            inverseJoinColumns = @JoinColumn(name = "permissionId")
-    )
-    private List<Permission> permission;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinTable(name = "role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permission> permission;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(
-            name = "role_menu",
-            joinColumns = @JoinColumn(name = "roleId"), // 改为蛇形命名
-            inverseJoinColumns = @JoinColumn(name = "menuId")
-    )
-    private List<Menu> menus;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "role_menu",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "menu_id"))
+    private Set<Menu> menus;
+    @ManyToOne
+    private User user;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Permission> getPermission() {
-        return permission;
-    }
-
-    public void setPermission(List<Permission> permission) {
-        this.permission = permission;
-    }
-
-    public List<Menu> getMenus() {
-        return menus;
-    }
-
-    public void setMenus(List<Menu> menus) {
-        this.menus = menus;
-    }
 }
