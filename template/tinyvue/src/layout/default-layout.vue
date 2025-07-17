c8cfd9ea4495a1c0d9345c28161088ffad309b80
chenxi <2465950588@qq.com>
2025-07-16 23:36:23 -0700

    feat: 优化查询表格页面，增加相应功能，修改pro整体边距布局

--- template/tinyvue/src/layout/default-layout.vue
+++ template/tinyvue/src/layout/default-layout.vue
@@ -9,7 +9,7 @@
           <Menu v-if="reloadKey !== 'menu' && layoutMode[myPattern].menu" class="shadow-[0_4px_12px_#0000001a] z-[100]" />
         </Suspense>
         <div class="text-[#ccc] bg-[#f5f6f7] pl-[10px] pr-[10px] flex-1 h-[calc(100vh-60px)]" :style="{ width: isMenuCollapsed ? '100%' : 'calc(100% - 220px)' }">
-          <PageLayout class="!h-[calc(100%-70px)]" />
+          <PageLayout class="!h-[calc(100%-70px)] pl-4 pr-4" />
           <Footer v-if="layoutMode[myPattern].footer" class="h-[60px]" />
         </div>
       </div>
