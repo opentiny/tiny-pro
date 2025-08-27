ff357884608bd4326f9e47947e368945b9058ad8
zzl12222 <161485392+zzl12222@users.noreply.github.com>
2025-08-27 15:56:57 +0800

    全局异常捕获 (#93)

--- template/springboot/src/main/java/com/TinyPro/exception/GlobalExceptionHandler.java
+++ template/springboot/src/main/java/com/TinyPro/exception/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.TinyPro.exception;
 
+import com.TinyPro.entity.contants.Contants;
 import com.TinyPro.utils.LocaleUntil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,20 +24,41 @@ public class GlobalExceptionHandler {
 
     }
 
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleException(BusinessException ex) {
-        // 根据错误码和参数获取国际化错误消息
-        String message = messageSource.getMessage(
-                ex.getErrorCode(),
-                new Object[]{ex.getArgs()},
-                LocaleUntil.getLocale()
-        );
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex) {
+        if (ex instanceof  BusinessException){
+            BusinessException exception=  (BusinessException) ex;
+            // 根据错误码和参数获取国际化错误消息
+            String message = messageSource.getMessage(
+                    exception.getErrorCode(),
+                    new Object[]{exception.getArgs()},
+                    LocaleUntil.getLocale()
+            );
 
-        // 创建自定义的错误响应对象，可以包含错误信息和状态码数值
-        ErrorResponse errorResponse = new ErrorResponse(message, ex.getHttpStatus().value());
+            // 创建自定义的错误响应对象，可以包含错误信息和状态码数值
+            ErrorResponse errorResponse = new ErrorResponse(message, exception.getHttpStatus().value());
 
-        return ResponseEntity
-                .status(ex.getHttpStatus())
-                .body(errorResponse);
+            return ResponseEntity
+                    .status(exception.getHttpStatus())
+                    .body(errorResponse);
+        } else if (ex instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException exception= (MethodArgumentNotValidException) ex;
+            String errorMsg = exception.getBindingResult()
+                    .getFieldErrors()
+                    .stream()
+                    .map(error -> MessageFormat.format(error.getDefaultMessage(),error.getField()))
+                    .collect(Collectors.joining(", "));
+            // 创建自定义的错误响应对象，可以包含错误信息和状态码数值
+            BusinessException e = new BusinessException(errorMsg, HttpStatus.BAD_REQUEST, null);
+            NoExistErrorResponse errorResponse = new NoExistErrorResponse(new String[]{errorMsg}, e.getHttpStatus().value(), Contants.NO_EXIST_ERROR_RESPONSE);
+
+            return ResponseEntity
+                    .status(e.getHttpStatus())
+                    .body(errorResponse);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("内部错误");
+        }
     }
 }
