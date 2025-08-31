92994aeef5b39ff7409f8b8611174ddd9cfdb686
GaoNeng-wWw <gaonenggg@outlook.com>
2025-08-31 10:57:10 +0800

    chore: change default page-size to `10`

--- template/tinyvue/src/views/role/info/components/info-tab.vue
+++ template/tinyvue/src/views/role/info/components/info-tab.vue
@@ -64,8 +64,8 @@
   }>({
     attrs: {
       currentPage: 1,
-      pageSize: 5,
-      pageSizes: [5, 10, 20, 50, 100],
+      pageSize: 10,
+      pageSizes: [10, 20, 50, 100],
       total: 0,
       align: 'right',
       layout: 'total, sizes, prev, pager, next, jumper'
