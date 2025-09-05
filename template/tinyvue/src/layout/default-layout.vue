36e3b4545f1c350ce2c6b079f7155e26b76aa0f3
Kagol <kagol@sina.com>
2025-09-05 16:19:21 +0800

    fix: fix tabs style

--- template/tinyvue/src/layout/default-layout.vue
+++ template/tinyvue/src/layout/default-layout.vue
@@ -14,6 +14,7 @@
             v-model="currentTabName"
             with-close
             size="small"
+            class="!pt-4"
             @click="onClick"
             @close="onClose"
           >
