61896c9c8564363b1b5432786b69e6bc1e019f0e
Ziwen <54743551+FoundDream@users.noreply.github.com>
2025-09-28 20:22:47 +0800

    feat: 新增低代码模版 (#111)

---
+++ template/lowcode-designer/vite.config.js
@@ -0,0 +1,36 @@
+import path from "node:path";
+import { defineConfig, mergeConfig } from "vite";
+import { useTinyEngineBaseConfig } from "@opentiny/tiny-engine-vite-config";
+
+export default defineConfig((configEnv) => {
+  const baseConfig = useTinyEngineBaseConfig({
+    viteConfigEnv: configEnv,
+    root: __dirname,
+    iconDirs: [
+      path.resolve(__dirname, "./node_modules/@opentiny/tiny-engine/assets/"),
+    ],
+    useSourceAlias: false,
+    envDir: "./env",
+    registryPath: "./registry.js",
+  });
+
+  const customConfig = {
+    envDir: "./env",
+    publicDir: path.resolve(__dirname, "./public"),
+    server: {
+      port: 8090,
+      proxy: {
+        "/auth": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
+        "/api": {
+          target: "http://localhost:3031",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+
+  return mergeConfig(baseConfig, customConfig);
+});
