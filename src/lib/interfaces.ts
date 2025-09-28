a736798427338069ab0273241d41413bc81c6a45
songziwen <songziwen.ai@bytedance.com>
2025-09-28 17:12:02 +0800

    feat: 支持初始化低代码设计器

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -20,6 +20,14 @@ export enum ServerFrameworks {
   Skip = '',
 }
 
+/**
+ * 低代码引擎选项
+ */
+export enum LowcodeEngine {
+  Include = 'lowcode-designer',
+  Skip = '',
+}
+
 export enum BuildTool {
   Vite = 'vite',
   Webpack = 'webpack',
@@ -118,6 +126,7 @@ export interface ProjectInfo {
   framework: string;
   name: string;
   serverFramework: ServerFrameworks;
+  lowcodeEngine?: LowcodeEngine;
   serverConfirm?: boolean;
   dialect?: string;
   host?: string;
