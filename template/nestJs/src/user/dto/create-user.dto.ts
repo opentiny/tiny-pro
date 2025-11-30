48e970e9720819dc906077ef68de5da80efa38c5
GaoNeng-wWw <gaonenggg@outlook.com>
2025-11-30 14:30:31 +0800

    doc: user openapi doc

--- template/nestJs/src/user/dto/create-user.dto.ts
+++ template/nestJs/src/user/dto/create-user.dto.ts
@@ -1,28 +1,52 @@
 import { IsNotEmpty } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 import { I18nTranslations } from '../../.generate/i18n.generated';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: '用户名称'
+  })
   @IsNotEmpty({
     message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
   })
   name: string;
+
+  @ApiProperty({
+    description: '用户邮箱, 登陆时用'
+  })
   @IsNotEmpty({
     message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
   })
   email: string;
+
+  @ApiProperty({
+    description: '用户密码, 登陆时用'
+  })
   @IsNotEmpty({
     message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
   })
   password: string;
+  @ApiProperty({
+    description: '用户角色'
+  })
   roleIds: number[] = [];
+  @ApiProperty({})
   department?: string;
+  @ApiProperty({})
   employeeType?: string;
+  @ApiProperty({})
   probationStart?: string;
+  @ApiProperty({})
   probationEnd?: string;
+  @ApiProperty({})
   probationDuration?: string;
+  @ApiProperty({})
   protocolStart?: string;
+  @ApiProperty({})
   protocolEnd?: string;
+  @ApiProperty({})
   address?: string;
+  @ApiProperty({})
   status?: number;
 }
