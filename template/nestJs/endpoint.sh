3de8df362030a7b3add4610a589e2fba4324ea74
GaoNeng-wWw <gaonenggg@outlook.com>
2025-11-30 21:37:33 +0800

    fix: add shebang

--- template/nestJs/endpoint.sh
+++ template/nestJs/endpoint.sh
@@ -1,1 +1,3 @@
-wait4x mysql "${DATABASE_USERNAME}:${DATABASE_PASSWORD}@tcp(mysql:3306)/${DATABASE_NAME}" --timeout=60s --interval=2s -- node ./migrate.js &&  node ./dist/main.js
+#!/bin/sh
+
+wait4x mysql "${DATABASE_USERNAME}:${DATABASE_PASSWORD}@tcp(mysql:3306)/${DATABASE_NAME}" --timeout=60s --interval=2s -- node ./migrate.js && node ./dist/main.js
