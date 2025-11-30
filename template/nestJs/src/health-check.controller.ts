6be7b5d8de93c81170be91e5261c33421f3d5499
GaoNeng-wWw <gaonenggg@outlook.com>
2025-11-30 13:20:55 +0800

    doc(nest.js): healthCheck swagger doc

--- template/nestJs/src/health-check.controller.ts
+++ template/nestJs/src/health-check.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { Public} from './public/public.decorator';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('healthCheck')
 export class HealthCheckController {
+  @ApiOkResponse({
+    type: 'string',
+    description: '确保健康检查能通过：`curl -k 127.0.0.1:3000/healthCheck` 获取到内容，而不是 404',
+    example: 'success'
+  })
   @Public()
   @Get()
   getEmptyResponse(): string {
-    return 'success'; // 确保健康检查能通过：`curl -k 127.0.0.1:3000/healthCheck` 获取到内容，而不是 404
+    return 'success';
   }
 }
