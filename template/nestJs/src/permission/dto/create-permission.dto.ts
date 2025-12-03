e50fd41216c8c3ad46176bc85ac6a01c7fa4f519
GaoNeng-wWw <gaonenggg@outlook.com>
2025-12-03 14:31:53 +0800

    doc(nestjs/permission): permission api doc

--- template/nestJs/src/permission/dto/create-permission.dto.ts
+++ template/nestJs/src/permission/dto/create-permission.dto.ts
@@ -1,12 +1,20 @@
 import { IsNotEmpty } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 import { I18nTranslations } from '../../.generate/i18n.generated';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePermissionDto {
+  @ApiProperty({
+    description: '权限在使用时候的键值对.'
+  })
   @IsNotEmpty({
     message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
   })
   name: string;
+
+  @ApiProperty({
+    description: '权限在后台, 人类可读的短简介'
+  })
   @IsNotEmpty({
     message: i18nValidationMessage<I18nTranslations>('validation.NOT_EMPTY'),
   })
