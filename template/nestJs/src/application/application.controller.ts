d2ea46032b7d2915b62f07f83e4eaa6b6d08e3a4
GaoNeng-wWw <gaonenggg@outlook.com>
2025-12-03 14:48:08 +0800

    doc(nestjs/application): implement application api doc by swagger

--- template/nestJs/src/application/application.controller.ts
+++ template/nestJs/src/application/application.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { PaginationQueryDto } from './dto/pagination-query.dto';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { FindApplicationEntity } from './entites/find-application.entity';
 
 @Controller('application')
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
 
+  @ApiOperation({summary: '获取应用'})
+  @ApiOkResponse({
+    type: FindApplicationEntity
+  })
   @Get()
   async getAllApplication(@Query() searchInfo: PaginationQueryDto) {
     return this.applicationService.findAllApplication(searchInfo);
