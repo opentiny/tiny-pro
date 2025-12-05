4220e703d8f3a95df26ee9dcaa37a5ee757de71a
GaoNeng <31283122+GaoNeng-wWw@users.noreply.github.com>
2025-12-05 23:13:27 +0800

    feat: add reject request flag (#146)
    * feat(nest.js): add preview mode flag
    
    * style: refine the preview mode warning tip
    
    * fix: use relative path

--- template/nestJs/src/public/reject.guard.ts
+++ template/nestJs/src/public/reject.guard.ts
@@ -8,11 +8,20 @@ import {
 import { Reflector } from '@nestjs/core';
 import { I18nTranslations } from '../.generate/i18n.generated';
 import { I18nContext } from 'nestjs-i18n';
+import { ConfigService } from '@nestjs/config';
+import { Configure } from '../config-schema';
 
 @Injectable()
 export class RejectRequestGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly cfg: ConfigService<Configure>
+  ) {
+  }
   async canActivate(ctx: ExecutionContext): Promise<boolean> {
+    if (!this.cfg.get('PREVIEW_MODE')) {
+      return true;
+    }
     const i18n = I18nContext.current<I18nTranslations>();
     const rejectRequest = this.reflector.getAllAndOverride('reject', [
       ctx.getHandler(),
