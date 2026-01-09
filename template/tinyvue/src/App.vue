768646285a88d508020e3e79d60c46fe8a17e8d3
Kagol <kagol@sina.com>
2026-01-09 15:49:23 +0800

    fix: remove useless log

--- template/tinyvue/src/App.vue
+++ template/tinyvue/src/App.vue
@@ -53,10 +53,10 @@
   };
 
   const sessionId = ref('')
-  onMounted(async () => {
-    const [serverTransport, clientTransport] = createMessageChannelPairTransport()
+  const [serverTransport, clientTransport] = createMessageChannelPairTransport()
+  provide('serverTransport', serverTransport)
 
-    provide('serverTransport', serverTransport)
+  onMounted(async () => {
 
     // 创建 WebMcpClient ，并与 WebAgent 连接
     const client = new WebMcpClient()
@@ -67,10 +67,9 @@
       // sessionId 为可选参数。若传入该参数，系统将使用指定值作为会话标识；若未传入，WebAgent 服务将自动生成一个随机的字符串作为 sessionId。为便于通过 MCP Inspector 工具进行调试，此处采用了固定的 sessionId。用户亦可通过浏览器原生提供的 crypto.randomUUID() 方法生成随机字符串作为会话标识。
       sessionId: 'd299a869-c674-4125-a84b-bb4e24079b99',
 
-      url: 'https://agent.opentiny.design/api/v1/webmcp-trial/mcp' // http://localhost:3005/api/v1/webmcp/mcp
+      url: 'https://agent.opentiny.design/api/v1/webmcp-trial/mcp'
     })
     sessionId.value = sessionID
-    console.log('sessionId:', sessionId.value)
   })
 </script>
 
