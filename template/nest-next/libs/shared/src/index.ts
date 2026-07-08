5a2d2ceb8a0893348885abb985de86d426f1d52c
GaoNeng-wWw <gaonenggg@outlook.com>
2026-07-08 14:33:50 +0800

    feat(nest-next): add i18n and exception filter

---
+++ template/nest-next/libs/shared/src/index.ts
@@ -0,0 +1,4 @@
+export { DomainError } from './error.base';
+export type { DomainErrorProps } from './error.base';
+export { GlobalExceptionFilter } from './global-exception.filter';
+
--- template/nest-next/src/app.module.ts
+++ template/nest-next/src/app.module.ts
@@ -1,26 +1,47 @@
 import { Module } from '@nestjs/common';
-import { ConfigureModule, ConfigureService } from '@app/configure';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { MySqlDriver } from '@mikro-orm/mysql';
+import { ConfigureModule } from '@app/configure';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from '@app/shared';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { join } from 'path';
+
+
 
 @Module({
   imports: [
     ConfigureModule,
-    MikroOrmModule.forRootAsync({
-      imports: [ConfigureModule],
-      inject: [ConfigureService],
-      useFactory: (configService: ConfigureService) => ({
-        entities: [],
-        host: configService.get('database.host'),
-        port: configService.get('database.port'),
-        driver: MySqlDriver,
-        user: configService.get('database.user'),
-        password: configService.get('database.password'),
-        dbName: configService.get('database.dbName'),
-      }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'enUS',
+      loaderOptions: {
+        path: join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [new HeaderResolver(['x-lang'])],
+      typesOutputPath: join(
+        __dirname,
+        '../libs/shared/src/.generate/i18n.generated.ts',
+      ),
     }),
+    // MikroOrmModule.forRootAsync({
+    //   imports: [ConfigureModule],
+    //   inject: [ConfigureService],
+    //   useFactory: (configService: ConfigureService) => ({
+    //     entities: [],
+    //     host: configService.get('database.host'),
+    //     port: configService.get('database.port'),
+    //     driver: MySqlDriver,
+    //     user: configService.get('database.user'),
+    //     password: configService.get('database.password'),
+    //     dbName: configService.get('database.dbName'),
+    //   }),
+    // }),
   ],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      useClass: GlobalExceptionFilter,
+    }
+  ],
 })
 export class AppModule {}
