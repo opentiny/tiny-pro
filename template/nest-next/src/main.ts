c603040499dc7e3c015649790a3640e19b52d776
GaoNeng-wWw <gaonenggg@outlook.com>
2026-07-21 15:11:58 +0800

    feat: winston nest logger

--- template/nest-next/src/main.ts
+++ template/nest-next/src/main.ts
@@ -1,3 +1,7 @@
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+import { utilities, WinstonModule } from 'nest-winston';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, VersioningType } from '@nestjs/common';
@@ -6,7 +10,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { I18nValidationPipe } from 'nestjs-i18n';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    logger: WinstonModule.createLogger({
+      instance: setupLogger(),
+    }),
+  });
   app.enableVersioning({
     type: VersioningType.MEDIA_TYPE,
     key: 'v=',
@@ -38,4 +47,52 @@ function setupSwagger(app: INestApplication, configure: SwaggerConfigure = {}) {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 }
+
+function setupLogger() {
+  return winston.createLogger({
+    // 日志选项
+    transports: [
+      new winston.transports.Console({
+        level: 'info',
+        // 字符串拼接
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+          utilities.format.nestLike(),
+        ),
+      }),
+      // warn、error日志存储到/logs/application-日期.log文件中
+      new winston.transports.DailyRotateFile({
+        level: 'warn',
+        dirname: 'logs',
+        filename: 'application-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.simple(),
+        ),
+      }),
+      // info日志存储到/logs/info-日期.log文件中
+      new winston.transports.DailyRotateFile({
+        level: 'info',
+        dirname: 'logs',
+        filename: 'info-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        // 文件大小
+        maxSize: '20m',
+        // 最多14 天
+        maxFiles: '14d',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.simple(),
+        ),
+      }),
+    ],
+  });
+}
+
 bootstrap().catch(console.error);
